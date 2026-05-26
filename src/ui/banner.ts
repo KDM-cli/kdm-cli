153dd26009b4e05999ec412eecb0dda5aad6c8fc
Yuvraj Sarathe <yuvrajsarathe07@gmail.com>
2026-05-26 22:41:29 +0530

    banner

--- src/ui/banner.ts
+++ src/ui/banner.ts
@@ -1,22 +1,22 @@
 import chalk from 'chalk';
 
 export const showWelcomeBanner = (version: string) => {
-  const banner = `
-  ${chalk.cyan('██╗  ██╗██████╗ ███╗   ███╗')}
-  ${chalk.cyan('██║ ██╔╝██╔══██╗████╗ ████║')}
-  ${chalk.cyan('█████╔╝ ██║  ██║██╔████╔██║')}
-  ${chalk.cyan('██╔═██╗ ██║  ██║██║╚██╔╝██║')}
-  ${chalk.cyan('██║  ██╗██████╔╝██║ ╚═╝ ██║')}
-  ${chalk.cyan('╚═╝  ╚═╝╚═════╝ ╚═╝     ╚═╝')}
-  `;
+  const banner = 
+  
+  
+  
+  
+  
+  
+  ;
 
-  const signature = chalk.gray(`
-  ──────────────────────────────────────────────────
-         ${chalk.white.bold('devloped by')} ${chalk.yellow.bold('utkarshpatrikar')}
-  ──────────────────────────────────────────────────
-  `);
+  const signature = chalk.gray(
+  --------------------------------------------------
+          
+  --------------------------------------------------
+  );
   
   console.log(banner);
   console.log(signature);
-  console.log(chalk.blue.bold(`  Kubernetes & Docker Monitor v${version}\n`));
+  console.log(chalk.blue.bold(  Kubernetes & Docker Monitor v\n));
 };
